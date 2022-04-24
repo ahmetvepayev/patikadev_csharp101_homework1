7129bbc3e6f9a7f300c25b863854bd9360aa9960
Ahmet Vepayev <ahmetwepaogly@gmail.com>
2022-04-24 17:02:16 +0300

    made helper methods more generic. reordered methods in Homework1 class

--- Program.cs
+++ Program.cs
@@ -8,13 +8,16 @@ namespace patikadev_csharp101_homework1
         {
             Homework1 homework1 = new Homework1();
 
-            while (true){
+            while (true)
+            {
                 Console.Write("_______Please enter the question number (enter \"0\" to exit) : ");
                 string input = Console.ReadLine();
-                if (input == "0"){
+                if (input == "0")
+                {
                     break;
                 }
-                switch (input){
+                switch (input)
+                {
                     case "1":
                         homework1.Question1();
                         break;
@@ -37,78 +40,108 @@ namespace patikadev_csharp101_homework1
 
     class Homework1
     {
-        public void Print(string text){
-            Console.Write(text);
-        }
-
-        public void PrintLine(string text){
-            Console.WriteLine(text);
-        }
-
-        public void PrintDivisible(int[] numbers, int factor){
-            foreach (int number in numbers){
-                if (number%factor == 0){
-                    number.PrintInteger();
-                }
-            }
-        }
-
-        public int ReadInteger(){
-            return int.Parse(Console.ReadLine());
-        }
-
-        public int[] ReadInteger(int n){
-            int[] numbers = new int[n];
-            for (int i = 0; i < n; i++){
-                numbers[i] = ReadInteger();
-            }
-            return numbers;
-        }
-
-        public void Question1(){
+        public void Question1()
+        {
             Print("Enter a positive integer: ");
-
-            int n = ReadInteger();
+            int n = ReadValue<int>();
             int[] numbers = new int[n];
 
             PrintLine($"Enter {n} positive integers. Press Enter after each integer: ");
-            numbers = ReadInteger(n);
+            numbers = ReadValues<int>(n);
 
             PrintLine("Even integers:");
             PrintDivisible(numbers, 2);
         }
 
-        public void Question2(){
+        public void Question2()
+        {
             PrintLine("Enter two positive integers. Press Enter after each integer:");
-            int n = ReadInteger();
-            int m = ReadInteger();
+            int n = ReadValue<int>();
+            int m = ReadValue<int>();
             int[] numbers = new int[n];
 
             PrintLine($"Enter {n} positive integers. Press Enter after each integer: ");
-            numbers = ReadInteger(n);
+            numbers = ReadValues<int>(n);
 
             PrintLine($"Integers divisible by {m}:");
             PrintDivisible(numbers, m);
         }
 
-        public void Question3(){
+        public void Question3()
+        {
             Console.Write("Question3");
         }
 
-        public void Question4(){
+        public void Question4()
+        {
             Console.Write("Question4");
         }
 
+
+        // -----------------Helper Methods----------------//
+        public void Print(string text)
+        {
+            Console.Write(text);
+        }
+
+        public void PrintLine(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void PrintDivisible(int[] numbers, int factor)
+        {
+            foreach (int number in numbers)
+            {
+                if (number%factor == 0)
+                {
+                    number.PrintInteger();
+                }
+            }
+        }
+
+        public string ReadValue()
+        {
+            return Console.ReadLine();
+        }
+
+        public T ReadValue<T>() where T: struct, IConvertible
+        {
+            return (T) Convert.ChangeType(Console.ReadLine(), typeof(T));
+        }
+
+        public string[] ReadValues(int n)
+        {
+            string[] words = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+            return words;
+        }
+
+        public T[] ReadValues<T>(int n) where T: struct, IConvertible
+        {
+            T[] values = new T[n];
+            for (int i = 0; i < n; i++)
+            {
+                values[i] = ReadValue<T>();
+            }
+            return values;
+        }
     }
 
     static class ExtensionMethods
     {
-        public static void PrintInteger(this int number){
+        public static void PrintInteger(this int number)
+        {
             Console.WriteLine(number);
         }
 
-        public static void PrintIntegerArray(this int[] numbers){
-            foreach (int number in numbers){
+        public static void PrintIntegerArray(this int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
                 number.PrintInteger();
             }
         }
