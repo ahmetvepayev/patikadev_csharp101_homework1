4805ad5426f3e2623440b866838c42afabf24aa2
Ahmet Vepayev <ahmetwepaogly@gmail.com>
2022-04-24 03:34:33 +0300

    implemented question1. implemented extension methods

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace patikadev_csharp101_homework1
             Homework1 homework1 = new Homework1();
 
             while (true){
-                Console.Write("Please enter the question number (enter \"0\" to exit) : ");
+                Console.Write("_______Please enter the question number (enter \"0\" to exit) : ");
                 string input = Console.ReadLine();
                 if (input == "0"){
                     break;
@@ -37,21 +37,59 @@ namespace patikadev_csharp101_homework1
 
     class Homework1
     {
+        public int ReadInteger(){
+            return int.Parse(Console.ReadLine());
+        }
+
+        public int[] ReadInteger(int n){
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++){
+                numbers[i] = ReadInteger();
+            }
+            return numbers;
+        }
+
         public void Question1(){
-            Console.WriteLine("Question1");
+            Console.Write("Enter a positive integer: ");
+
+            int n = ReadInteger();
+            int[] numbers = new int[n];
+
+            Console.WriteLine($"Enter {n} positive integers. Press Enter after each integer: ");
+            numbers = ReadInteger(n);
+
+            Console.WriteLine("Even integers:");
+            foreach (int number in numbers){
+                if (number%2 == 0){
+                    number.PrintInteger();
+                }
+            }
         }
 
         public void Question2(){
-            Console.WriteLine("Question2");
+            Console.Write("Question2");
         }
 
         public void Question3(){
-            Console.WriteLine("Question3");
+            Console.Write("Question3");
         }
 
         public void Question4(){
-            Console.WriteLine("Question4");
+            Console.Write("Question4");
         }
 
+    }
+
+    static class ExtensionMethods
+    {
+        public static void PrintInteger(this int number){
+            Console.WriteLine(number);
+        }
+
+        public static void PrintIntegerArray(this int[] numbers){
+            foreach (int number in numbers){
+                number.PrintInteger();
+            }
+        }
     }
 }
